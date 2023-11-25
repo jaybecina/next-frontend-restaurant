953baa9a73f4bdc1809167c2f8fad81265be2271
giuseppe jay becina <jaybecina@gmail.com>
2023-11-25 21:43:13 +0800

    Changed API URLs for local and production

--- src/app/services/restaurantService.ts
+++ src/app/services/restaurantService.ts
@@ -3,7 +3,7 @@ import { API_URL } from "../lib/envBaseURL";
 
 // Fetch data from the API here
 const getAllRestaurant = async () => {
-  return await axios.get(`${API_URL}`);
+  return await axios.get(`${API_URL}/api/restaurants`);
 };
 
 export { getAllRestaurant };
